d942e14819b2a484523b06648cc3eb34be120471
Alfonso Vacas <alfonsovacas1979@gmail.com>
2020-09-24 16:42:38 +0200

    curso javascript desde 0

---
+++ DOM/scripts.js
@@ -0,0 +1,9 @@
+const title = document.getElementById('title');
+const name = document.getElementById('name');
+
+console.log(title);
+
+
+//console.log(name.getAttribute('type'));
+name.setAttribute('type', 'date');
+console.log(name);
